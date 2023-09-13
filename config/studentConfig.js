81eb317a199e6440d3847a109d47faead6fd49c7
Sifiso Mawila <sfisonxumalo69@gmail.com>
2023-09-13 09:45:10 +0200

    updated configs/newDb

--- config/studentConfig.js
+++ config/studentConfig.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db_url : 'mongodb://127.0.0.1:27017/mydb'
+    db_url : 'mongodb://127.0.0.1:27017/myStudents',
+    db_port : 3865
 }
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const DB = require("./config/studentConfig")
 const studentRoutes = require('./routes/studentRoutes');
+const configData = require("./config/studentConfig")
 
 const app = express();
-const port = 3865;
+const port = configData.db_port;
 
  
 mongoose.connect(DB.db_url, {
