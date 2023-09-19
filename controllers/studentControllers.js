338623ed915219e4926aee1b8699de390018bcd4
Sifiso Mawila <sfisonxumalo69@gmail.com>
2023-09-19 10:02:48 +0200

    update

--- controllers/studentControllers.js
+++ controllers/studentControllers.js
@@ -129,7 +129,7 @@ exports.updateStudents = async(req, res) => {
     try {
         
         const id = req.params.id;
-        const students = await Student.findByIdAndUpdate(id, req.body);
+        const students = await Student.findOneAndUpdate({studentNumber:id}, req.body);
         res.status(200).json(students);
       } catch (error) {
         res.status(500).json({ error: error.message });
